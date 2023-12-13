ec58a8decf5546e08f759bbff60d6c1036c9eb1a
user1 <teleginsky@mail.ru>
2023-12-14 02:04:47 +0300

    Обновил файл Vlad.py

--- Part1/Vlad.py
+++ Part1/Vlad.py
@@ -1,1 +1,5 @@
-# Телегинский Владислав Владиславович
+#Переводчик с английского на русский
+from transformers import pipeline
+model_en2ru = pipeline("translation_en_to_ru", model = "Helsinki-NLP/opus-mt-en-ru")
+result = model_en2ru("I am student")
+result
