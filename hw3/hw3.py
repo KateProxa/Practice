cab1d653af2e6552c481ec51b2112bfad6a2b9dd
anvastol@yandex.ru <anvastol@yandex.ru>
2023-12-23 17:16:03 +0300

    new code

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,17 +1,2 @@
 from fastapi import FastAPI
-from transformers import pipeline
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    text: str
-
-app = FastAPI()
-classifier = pipeline("sentiment-analysis")
-
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-@app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text )[0]
+from transformers import pipel
