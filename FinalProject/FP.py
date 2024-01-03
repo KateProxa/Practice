47f8b50ab325bc4b4f3c5c76475708a8c0e1b5a8
anvastol@yandex.ru <anvastol@yandex.ru>
2024-01-03 20:44:13 +0300

    Добавление кода в файл FP.py

--- FinalProject/FP.py
+++ FinalProject/FP.py
@@ -1,1 +1,14 @@
-1
+import io
+import streamlit as st
+from transformers import pipeline
+@st.cache_resource
+def load_model():
+    return pipeline("translation_en_to_de", model = "Helsinki-NLP/opus-mt-en-de")
+translation = load_model()
+st.title('Translator to Deutch')
+st.header('Это приложение для перевода текста на немецкий язык')
+text = st.text_input('Введите текст для перевода на английском языке')
+result = st.button('Перевести')
+if result:
+    tr_text = translation(text)
+    st.write("Перевод:", tr_text[0]['translation_text'])
