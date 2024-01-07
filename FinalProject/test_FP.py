abe45d3b01ce6163099134136de02b9119811302
user1 <teleginsky@mail.ru>
2024-01-07 19:34:03 +0300

    Add test

---
+++ FinalProject/test_FP.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+
+def model(text):
+    model_en2de = pipeline("translation_en_to_de", model = "Helsinki-NLP/opus-mt-en-de")
+    return (model_en2de(text))
+
+def test_1():
+     assert model("I am student") == [{'translation_text': 'Ich bin Studentin'}]
