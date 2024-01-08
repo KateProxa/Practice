71204006ef661ea4bd14dd75b62e32df1a7b9ecc
KateProxa <ekater_1997@mail.ru>
2024-01-08 18:30:34 +0300

    Add files for test

--- Part5/Translator/test_main.py
+++ Part5/Translator/test_main.py
@@ -3,7 +3,17 @@ from main import app
 
 client = TestClient(app)
 
-def test_read_main():
-    response = client.post("/predict/'")
+def test_empty_text():
+    response = client.post("/predict/", json={"text": ""})
+    json_data = response.json()
     assert response.status_code == 200
-    #assert response.json() == {"text": "Скоро наступит Новый Год!"}
+    assert json_data['translation_text'] == '- Je sais.'
+
+def test_empty_json():
+    response = client.post("/predict/", json={})
+    json_data = response.json()
+    assert response.status_code == 422
+    assert json_data['detail'][0]['type'] == "missing"
+    assert json_data['detail'][0]['msg'] == "Field required"
+
+
