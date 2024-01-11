063157779ca1786359d4e6429b01f997bad51b8c
xobi_van <xobi_van@Linux.myguest.virtualbox.org>
2024-01-11 23:23:22 +0000

    Added Dima.py

---
+++ Part1/Dima.py
@@ -0,0 +1,5 @@
+#Переводчик с русского на французский
+from transformers import pipeline
+model_ru2fr = pipeline("translation_ru_to_fr", model = "Helsinki-NLP/opus-mt-ru-fr")
+result = model_ru2fr("всем привет меня зовут Дмитрий")
+# Salut, tout le monde. Mon nom est Dimitri.
