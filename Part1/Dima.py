3dfb837c71b196aca7b05a24a0826cf1f7a71b67
xobivan <71180245+xobivan@users.noreply.github.com>
2024-01-11 23:40:09 +0000

    Update Dima.py

--- Part1/Dima.py
+++ Part1/Dima.py
@@ -2,4 +2,5 @@
 from transformers import pipeline
 model_ru2fr = pipeline("translation_ru_to_fr", model = "Helsinki-NLP/opus-mt-ru-fr")
 result = model_ru2fr("всем привет меня зовут Дмитрий")
+result
 # Salut, tout le monde. Mon nom est Dimitri.
