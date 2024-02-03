8b5f2d9ce4ee320af6cd62688fafef46b12130ac
xobi_van <xobi_van@Linux.myguest.virtualbox.org>
2024-02-03 15:59:59 +0000

    Added Dmitry folder

---
+++ Part3/Dmitry/translator.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+
+app = FastAPI
+
+
+class Item(BaseModel):
+    text: str
+
+translate = pipeline("translation", model="Helsinki-NLP/opus-mt-ru-fr")
+
+@app.post("/predict/")
+async def predict(item:Item):
+    return translate(item.text)[0]
